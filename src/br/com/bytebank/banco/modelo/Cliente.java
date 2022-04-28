ffa69c8627b65386b7405e62342950078dea4bc2
leandrozulis <leandro_zulli@hotmail.com>
2022-04-28 15:27:48 -0300

    javadoc

--- src/br/com/bytebank/banco/modelo/Cliente.java
+++ src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,5 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe que representa um cliente no Bytebank.
+ *
+ * @author Leandro Zulli
+ * @version 0.1
+ */
+
 public class Cliente {
 
     private String nome;
