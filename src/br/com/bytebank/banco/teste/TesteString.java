f6eb328eb0cac68f5b761e26a143ddf0d12afd4a
leandrozulis <leandro_zulli@hotmail.com>
2022-04-28 22:46:56 -0300

    'java.lang'

---
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,42 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+	
+	public static void main(String[] args) {
+		
+//		String vazio = " ";
+//		String outroVazio = vazio.trim(); // Remove os espaços do começo e fim
+//		System.out.println(outroVazio.isEmpty()); // Retorna se a String está vazia ou não
+//		System.out.println(vazio.contains("Alu")); // Retorna se contem essa sequência de caracteres dentro do objeto
+//		
+		String nome = "Alura"; // object literal
+//		//String outro = new String("Alura");
+//		
+//		System.out.println(nome.length());
+//		
+//		for(int i = 0; i < nome.length(); i++) {
+//			System.out.println(nome.charAt(i));
+//		}
+		
+//		String sub = nome.substring(1); // Retorna após a primeira posição começando em 1
+//		System.out.println(sub);
+		
+//		int pos = nome.indexOf("lura"); // Retorna o índice da String
+//		System.out.println(pos);
+		
+//		char c = nome.charAt(2); // Retorna a partir do índice 2 o caractere
+//		System.out.println(c);
+		
+//		char c = 'A';
+//		char d = 'a';
+	
+//		String outro = nome.replace(c, d);
+		
+//		String outro = nome.toUpperCase();
+		
+//		System.out.println(nome);
+//		System.out.println(outro);
+		
+	}
+
+}
